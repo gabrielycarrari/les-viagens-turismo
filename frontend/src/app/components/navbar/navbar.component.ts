be01f66418a0c1a66aefb546925212dfcff167fd
Gabriely Carrari <gabymcarrari@gmail.com>
2024-04-22 13:22:59 -0300

    :sparkles: feat: pagina home

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router) {}
+
+  public login() {
+    this.router.navigate(['/login']);
+  }
 
 }
