cc18b1a9a28b2ae5be377ed74abbfdfccd888a93
Gabriely Carrari <gabymcarrari@gmail.com>
2024-05-02 13:35:25 -0300

    correção merge - add rota login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardClientesComponent } from './components/dashboard-clientes/dashboard-clientes.component';
-
-
 import { CadastroClienteComponent } from './components/cliente/cadastro-cliente/cadastro-cliente.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
   },
   {
     path: "dashboard",
